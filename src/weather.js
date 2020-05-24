2f88b34d67d320ce278abecbfa880c9441f248a0
Jan Zak <zj@zakjan.cz>
2020-05-24 21:27:36 +0200

    Init screen textures with empty data, to get rid of Firefox warning

--- src/weather.js
+++ src/weather.js
@@ -58,8 +58,9 @@ export async function drawWeather(canvas, config) {
             canvas.height = canvas.parentElement.clientHeight * pixelRatio;
         }
 
-        particlesScreenTexture0 = createArrayTexture(gl, null, canvas.width, canvas.height);
-        particlesScreenTexture1 = createArrayTexture(gl, null, canvas.width, canvas.height);
+        const emptyTexture = new Uint8Array(gl.canvas.width * gl.canvas.height * 4);
+        particlesScreenTexture0 = createArrayTexture(gl, emptyTexture, gl.canvas.width, gl.canvas.height);
+        particlesScreenTexture1 = createArrayTexture(gl, emptyTexture, gl.canvas.width, gl.canvas.height);
     }
     function initResizeObserver() {
         if (canvas.parentElement) {
@@ -104,13 +105,11 @@ export async function drawWeather(canvas, config) {
         gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
         gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, particlesStateTexture1.texture, 0);
         gl.viewport(0, 0, particlesStateTexture0.x, particlesStateTexture0.y);
-        gl.clear(gl.COLOR_BUFFER_BIT);
         computeStep(gl, stepProgram, quadBuffer, particlesStateTexture0, weatherMetadata, weatherTexture, config.speedFactor, config.dropRate, config.dropRateBump);
 
         // draw to particles screen texture
         gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, particlesScreenTexture1.texture, 0);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-        gl.clear(gl.COLOR_BUFFER_BIT);
         drawFade(gl, fadeProgram, quadBuffer, particlesScreenTexture0, config.fadeOpacity);
         drawParticles(gl, particlesProgram, particlesBuffer, particlesStateTexture0, particlesStateTexture1, particleSize, particleColor);
 
