daa6cc2e0a774c82f5709f7f18e0fb61b634caf2
Jan Zak <zj@zakjan.cz>
2020-06-02 07:40:24 +0200

    Use GDAL for transforming the source data

--- data/gfs/download.sh
+++ data/gfs/download.sh
@@ -14,11 +14,20 @@ BBOX="leftlon=0&rightlon=360&toplat=90&bottomlat=-90"
 URL="https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_${RESOLUTION}.pl?file=gfs.t${TIME}z.pgrb2.${RESOLUTION}.${OFFSET}&${LEVEL}&${VARIABLES}&${BBOX}&dir=%2Fgfs.${DATE}%2F${TIME}"
 
 LAYER_FILENAME_PREFIX="${DIR}/${DATE}${TIME}"
-if [ ! -f "${LAYER_FILENAME_PREFIX}.grib.json" ]; then
+if [ ! -f "${LAYER_FILENAME_PREFIX}.grib" ]; then
     curl "${URL}" -o "${LAYER_FILENAME_PREFIX}.grib"
-    grib_dump -j "${LAYER_FILENAME_PREFIX}.grib" > "${LAYER_FILENAME_PREFIX}.grib.json"
 fi
 
-node "${DIR}/prepare.js" "${LAYER_FILENAME_PREFIX}.grib.json" "${LAYER_FILENAME_PREFIX}.png"
-
-# rm "${LAYER_FILENAME_PREFIX}.grib" "${LAYER_FILENAME_PREFIX}.grib.json"
+gdalwarp \
+    --config CENTER_LONG 0 \
+    "${LAYER_FILENAME_PREFIX}.grib" \
+    "${LAYER_FILENAME_PREFIX}.tif"
+gdal_translate \
+    -ot Byte \
+    -b 1 -b 2 -b 2 \
+    -scale_1 -128 127 0 255 \
+    -scale_2 -128 127 0 255 \
+    -scale_3 -128 127 0 0 \
+    --config GDAL_PAM_ENABLED NO \
+    "${LAYER_FILENAME_PREFIX}.tif" \
+    "${LAYER_FILENAME_PREFIX}.png"
