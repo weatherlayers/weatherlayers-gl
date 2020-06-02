f42de62e252e0668292590ff1ca8c3b35ab80cd7
Jan Zak <zj@zakjan.cz>
2020-06-02 11:48:11 +0200

    Scale control

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { drawToCanvas } from './canvas.js';
 export { drawToGl } from './gl.js';
+export { ScaleControl } from './mapbox-scale-control.js';
---
+++ src/mapbox-scale-control.js
@@ -0,0 +1,48 @@
+export class ScaleControl {
+    /**
+     * @param {[number, number, number][]} colorRamp
+     */
+    constructor(colorRamp) {
+        this.colorRamp = colorRamp;
+    }
+
+    /**
+     * @param {mapboxgl} map
+     */
+    onAdd(map) {
+        this.map = map;
+        this.container = document.createElement('div');
+        this.container.className = 'mapboxgl-ctrl';
+
+        const canvas = this.createCanvas();
+        this.container.appendChild(canvas);
+
+        return this.container;
+    }
+
+    onRemove() {
+        if (this.container && this.container.parentNode) {
+            this.container.parentNode.removeChild(this.container);
+            this.container = undefined;
+        }
+        this.map = undefined;
+    }
+
+    /**
+     * @return HTMLCanvasElement
+     */
+    createCanvas() {
+        const canvas = /** @type HTMLCanvasElement */ (document.createElement('canvas'));
+        canvas.width = 256;
+        canvas.height = 10;
+        canvas.style.imageRendering = "-moz-crisp-edges";
+        canvas.style.imageRendering = "pixelated";
+        const ctx = /** @type CanvasRenderingContext2D */ (canvas.getContext('2d'));
+        this.colorRamp.forEach((color, i) => {
+            ctx.fillStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+            ctx.fillRect(i, 0, 1, 10);
+        });
+
+        return canvas;
+    }
+}
