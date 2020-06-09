380d89b04d1e460d7e7ce519bcb109ba3ca93826
Jan Zak <zj@zakjan.cz>
2020-06-09 09:22:25 +0200

    Add comments

--- data/gfs/download.sh
+++ data/gfs/download.sh
@@ -14,14 +14,23 @@ BBOX="leftlon=0&rightlon=360&toplat=90&bottomlat=-90"
 URL="https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_${RESOLUTION}.pl?file=gfs.t${TIME}z.pgrb2.${RESOLUTION}.${OFFSET}&${LEVEL}&${VARIABLES}&${BBOX}&dir=%2Fgfs.${DATE}%2F${TIME}"
 
 LAYER_FILENAME_PREFIX="${DIR}/${DATE}${TIME}"
+
+# download GFS file
 if [ ! -f "${LAYER_FILENAME_PREFIX}.grib" ]; then
     curl "${URL}" -o "${LAYER_FILENAME_PREFIX}.grib"
 fi
 
+# map longitude from 0..360 to -180..180
 gdalwarp \
     --config CENTER_LONG 0 \
     "${LAYER_FILENAME_PREFIX}.grib" \
     "${LAYER_FILENAME_PREFIX}.tif"
+
+# transform into PNG image texture
+# map values from -128..127 float to 0..255 uint8
+# R channel - band 1 (u wind vector)
+# G channel - band 2 (v wind vector)
+# B channel - empty
 gdal_translate \
     -ot Byte \
     -b 1 -b 2 -b 2 \
