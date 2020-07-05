cfd8999297690a9578e8f5ac27e35a25bad496ce
Jan Zak <zj@zakjan.cz>
2020-07-05 09:58:51 +0200

    Revert "Try native texture filtering"
    This reverts commit 89775af427a0645ffb1d78f92fca6498822851e5.

--- src/shaders/_get-position-values.glsl
+++ src/shaders/_get-position-values.glsl
@@ -3,8 +3,8 @@
 vec4 getPositionValues(sampler2D texture, vec2 resolution, vec2 position) {
     vec2 wrappedPosition = vec2(fract(position.x), position.y);
 
-    vec4 values = texture2D(texture, wrappedPosition); // lower-res hardware linear filtering
-    // vec4 values = texture2DBilinear(texture, resolution, wrappedPosition);
+    // vec2 values = texture2D(texture, wrappedPosition); // lower-res hardware linear filtering
+    vec4 values = texture2DBilinear(texture, resolution, wrappedPosition);
 
     return values;
 }
