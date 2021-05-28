f015908032d613b8138fb2da0809fafd40e849e7
Jan Zak <zj@zakjan.cz>
2021-05-28 06:40:19 +0200

    Cleanup

--- docs/legend-control.js
+++ docs/legend-control.js
@@ -1,19 +1,13 @@
 export class LegendControl {
-  constructor(config) {
-    this.config = config;
-  }
-
   onAdd() {
     this.container = document.createElement('div');
-    this.container.className = 'mapboxgl-ctrl';
+    this.container.className = 'mapboxgl-ctrl legend';
     this.container.style.fontFamily = '"Helvetica Neue", Arial, Helvetica, sans-serif';
     this.container.style.fontSize = '10px';
     this.container.style.background = 'rgba(255, 255, 255, 0.5)';
     this.container.style.backdropFilter = 'blur(1px)';
     this.container.style.fontSize = '10px';
     this.container.style.color = '#333';
-
-    this.update();
 
     return this.container;
   }
@@ -25,7 +19,7 @@ export class LegendControl {
     }
   }
 
-  update() {
+  update(config) {
     if (!this.container) {
       return;
     }
@@ -35,13 +29,13 @@ export class LegendControl {
     const xmlns = 'http://www.w3.org/2000/svg';
     const paddingY = 15;
     const svg = /** @type SVGElement */ (document.createElementNS(xmlns, 'svg'));
-    svg.setAttribute('width', `${this.config.legendWidth + 2 * paddingY}px`);
+    svg.setAttribute('width', `${config.legendWidth + 2 * paddingY}px`);
     svg.setAttribute('height', '50px');
     svg.style.display = 'block';
     this.container.appendChild(svg);
 
     const title = /** @type SVGTextElement */ (document.createElementNS(xmlns, 'text'));
-    title.innerHTML = this.config.legendTitle;
+    title.innerHTML = config.legendTitle;
     title.style.fontWeight = 'bold';
     title.style.transform = `translate(${paddingY}px, 15px)`;
     svg.appendChild(title);
@@ -51,8 +45,8 @@ export class LegendControl {
     svg.appendChild(scale);
 
     const image = /** @type SVGImageElement */ (document.createElementNS(xmlns, 'image'));
-    image.setAttribute('href', this.config.colormapUrl);
-    image.setAttribute('width', `${this.config.legendWidth}`);
+    image.setAttribute('href', config.colormapUrl);
+    image.setAttribute('width', `${config.legendWidth}`);
     image.setAttribute('height', '5');
     image.setAttribute('preserveAspectRatio', 'none');
     scale.appendChild(image);
@@ -61,15 +55,15 @@ export class LegendControl {
     ticks.style.textAnchor = 'middle';
     scale.appendChild(ticks);
 
-    const bounds = this.config.colorBounds;
-    const delta = (bounds[1] - bounds[0]) / (this.config.legendTicksCount - 1);
-    for (let i = 0; i < this.config.legendTicksCount; i++) {
+    const bounds = config.colorBounds;
+    const delta = (bounds[1] - bounds[0]) / (config.legendTicksCount - 1);
+    for (let i = 0; i < config.legendTicksCount; i++) {
       const value = bounds[0] + i * delta;
-      const formattedValue = this.config.legendValueFormat?.(value) ?? value;
-      const roundedFormattedValue = this.config.legendValueDecimals ? Math.round(formattedValue * 10 ** this.config.legendValueDecimals) / 10 ** this.config.legendValueDecimals : Math.round(formattedValue);
+      const formattedValue = config.legendValueFormat?.(value) ?? value;
+      const roundedFormattedValue = config.legendValueDecimals ? Math.round(formattedValue * 10 ** config.legendValueDecimals) / 10 ** config.legendValueDecimals : Math.round(formattedValue);
 
       const tick = /** @type SVGGElement */ (document.createElementNS(xmlns, 'g'));
-      tick.style.transform = `translate(${(value - bounds[0]) / (bounds[1] - bounds[0]) * this.config.legendWidth}px, 0)`;
+      tick.style.transform = `translate(${(value - bounds[0]) / (bounds[1] - bounds[0]) * config.legendWidth}px, 0)`;
       ticks.appendChild(tick);
 
       const tickLine = /** @type SVGLineElement */ (document.createElementNS(xmlns, 'line'));
