5e51328cab0bf8ed45b292c20eb3c99866030a51
Jan Zak <zj@zakjan.cz>
2021-05-29 10:33:44 +0200

    Fix Antarctica polygon #2

--- docs/data.js
+++ docs/data.js
@@ -45,27 +45,51 @@ export async function fixMapGeoJsonUrl(url) {
   return blobUrl;
 }
 
-export async function fixGlobeGeoJsonUrl(url) {
+export async function getLandUrl() {
+  const isPointValid = point => -85.051129 < point[1] && point[1] < 85.051129;
+
+  const url = 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_land.geojson';
   const geojson = await (await fetch(url)).json();
 
-  geojson.features.forEach(feature => {
+  // transform polygons to lines
+  geojson.features = geojson.features.map(feature => {
+    let newFeature;
     if (feature.geometry.type === 'Polygon') {
-      feature.geometry.coordinates = feature.geometry.coordinates.map(polygon => {
-        const filteredPolygon = polygon.filter(point => -85.051129 < point[1] && point[1] < 85.051129);
-        if (polygon.length !== filteredPolygon.length) {
-          const fixedPolygon = filteredPolygon.map(point => {
-            return [
-              ...(point[0] === -180 ? [[point[0], Math.sign(point[1]) * 90]] : []),
-              point,
-              ...(point[0] === 180 ? [[point[0], Math.sign(point[1]) * 90]] : []),
-            ];
-          }).flat();
-          return fixedPolygon;
-        } else {
-          return polygon;
-        }
-      });
+      newFeature = {
+        ...feature,
+        geometry: {
+          type: 'MultiLineString',
+          coordinates: feature.geometry.coordinates.map(line => {
+            return line.filter(isPointValid);
+          }),
+        },
+      };
+    } else if (feature.geometry.type === 'MultiPolygon') {
+      newFeature = {
+        ...feature,
+        geometry: {
+          type: 'MultiLineString',
+          coordinates: feature.geometry.coordinates.map(polygon => {
+            return polygon.map(line => {
+              return line.filter(isPointValid);
+            });
+          }).flat(),
+        },
+      };
+    } else {
+      newFeature = feature;
     }
+      
+    const splitAntarctica = feature.geometry.coordinates.length === 1 && feature.geometry.coordinates[0].length !== newFeature.geometry.coordinates[0].length;
+    if (splitAntarctica) {
+      const index = newFeature.geometry.coordinates[0].findIndex(point => point[0] === -180);
+      newFeature.geometry.coordinates = [
+        newFeature.geometry.coordinates[0].slice(0, index),
+        newFeature.geometry.coordinates[0].slice(index),
+      ];
+    }
+
+    return newFeature;
   });
 
   const blob = new Blob([JSON.stringify(geojson)], { type: 'application/json' });
