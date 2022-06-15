70c95bb2c8e529c208992ced9790adcd3a9f1f75
Jan Zak <zj@zakjan.cz>
2022-06-15 08:40:31 +0200

    Add ContourGpu demo

--- src/deck/index.js
+++ src/deck/index.js
@@ -2,7 +2,7 @@ export * from '../_utils/index';
 
 export * from './layers/raster-layer/raster-layer';
 export * from './layers/contour-layer/contour-layer';
-// export * from './layers/contour-layer/contour-gpu-layer';
+export * from './layers/contour-layer/contour-gpu-layer';
 export * from './layers/high-low-layer/high-low-layer';
 export * from './layers/grid-layer/grid-layer';
 export * from './layers/particle-layer/particle-layer';
