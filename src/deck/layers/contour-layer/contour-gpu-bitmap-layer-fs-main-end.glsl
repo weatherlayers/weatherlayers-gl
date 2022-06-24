4c6d2449a9617f1cf929af50067bc057b273cb36
Jan Zak <zj@zakjan.cz>
2022-06-25 00:08:53 +0200

    Improve GPU contours, add major contours

--- src/deck/layers/contour-layer/contour-gpu-bitmap-layer-fs-main-end.glsl
+++ src/deck/layers/contour-layer/contour-gpu-bitmap-layer-fs-main-end.glsl
@@ -10,8 +10,16 @@ if (!raster_has_values(bitmapColor)) {
 }
 
 float value = raster_get_value(bitmapColor);
-float contourValueFract = abs(fract(value / interval) - 0.5) * 2.;
-float contourValueFwidth = fwidth(value / interval);
-float contourValue = 1. - smoothstep(contourValueFwidth * 1., contourValueFwidth * 2., contourValueFract);
-vec4 rasterColor = vec4(color.rgb, color.a * contourValue);
-gl_FragColor = raster_apply_opacity(rasterColor.rgb, rasterColor.a * rasterOpacity);
+float contourValue = value / interval;
+float major = step(fract(contourValue * 0.2), 0.1); // 1: major contour every fifth contour, 0: minor contour
+float contourWidth = width * (1. + major) - 0.5; // major contour: thicker
+
+// https://stackoverflow.com/a/30909828/1823988
+// https://forum.unity.com/threads/antialiased-grid-lines-fwidth.1010668/
+// https://www.shadertoy.com/view/Mlfyz2
+float factor = abs(fract(contourValue + 0.5) - 0.5); // contour position, min 0: contour, max 0.5: between contours
+float dfactor = fwidth(contourValue); // contour derivation, consistent width in screen space
+float contourOpacity = 1. - clamp(factor / dfactor - contourWidth, 0., 1.);
+
+// contourOpacity += factor; // debug
+gl_FragColor = vec4(color.rgb, color.a * contourOpacity * rasterOpacity);
