4449a07ac14f639b5936c2a9a29e8c65df0f3074
Jan Zak <zj@zakjan.cz>
2022-08-22 22:01:25 +0200

    Update spacing

--- src/_controls/legend-control/legend-control.js
+++ src/_controls/legend-control/legend-control.js
@@ -89,33 +89,32 @@ export class LegendControl {
     const div = document.createElement('div');
     this.container.appendChild(div);
 
+    const header = document.createElement('header');
+    div.appendChild(header);
+
+    const text = document.createElement('span');
+    text.className = 'text';
+    text.innerHTML = `${title} [${formatUnit(unit)}]`;
+    header.appendChild(text);
+
+    const main = document.createElement('main');
+    div.appendChild(main);
+
     const xmlns = 'http://www.w3.org/2000/svg';
     const svg = document.createElementNS(xmlns, 'svg');
-    svg.setAttribute('width', `${width}px`);
-    svg.setAttribute('height', '56px');
-    svg.style.verticalAlign = 'middle';
-    div.appendChild(svg);
-
-    const text = document.createElementNS(xmlns, 'text');
-    text.innerHTML = `${title} [${formatUnit(unit)}]`;
-    text.style.fontWeight = 'bold';
-    text.style.transform = `translate(${PADDING_Y}px, 17px)`;
-    svg.appendChild(text);
+    svg.setAttribute('height', '24px');
+    svg.setAttribute('class', 'legend');
+    main.appendChild(svg);
 
     const g = document.createElementNS(xmlns, 'g');
-    g.style.transform = `translate(${PADDING_Y}px, 28px)`;
     svg.appendChild(g);
 
     const image = document.createElementNS(xmlns, 'image');
     image.setAttribute('href', paletteCanvasDataUrl);
-    image.setAttribute('width', `${width - 2 * PADDING_Y}`);
+    image.setAttribute('width', '100%');
     image.setAttribute('height', '5');
     image.setAttribute('preserveAspectRatio', 'none');
     g.appendChild(image);
-
-    const ticks = document.createElementNS(xmlns, 'g');
-    ticks.style.textAnchor = 'middle';
-    g.appendChild(ticks);
 
     const delta = (paletteBounds[1] - paletteBounds[0]) / (ticksCount - 1);
     for (let i = 0; i < ticksCount; i++) {
@@ -123,22 +122,30 @@ export class LegendControl {
       const formattedValue = formatValue(value, unit);
 
       const tick = document.createElementNS(xmlns, 'g');
-      tick.style.transform = `translate(${(value - paletteBounds[0]) / (paletteBounds[1] - paletteBounds[0]) * (width - 2 * PADDING_Y)}px, 0)`;
-      ticks.appendChild(tick);
+      tick.style.transform = `translate(${(value - paletteBounds[0]) / (paletteBounds[1] - paletteBounds[0]) * 100}%, 0)`;
+      g.appendChild(tick);
 
       const tickLine = document.createElementNS(xmlns, 'line');
       tickLine.setAttribute('y1', '0');
       tickLine.setAttribute('y2', '10');
       tickLine.style.stroke = 'currentColor';
+      if (i === 0) {
+        tickLine.style.transform = 'translate(0.5px, 0)';
+      } else if (i === ticksCount - 1) {
+        tickLine.style.transform = 'translate(-0.5px, 0)';
+      }
       tick.appendChild(tickLine);
 
       const tickValue = document.createElementNS(xmlns, 'text');
       tickValue.innerHTML = formattedValue;
-      tickValue.style.transform = 'translate(0, 22px)';
+      tickValue.setAttribute('x', '0');
+      tickValue.setAttribute('y', '22');
       if (i === 0) {
         tickValue.style.textAnchor = 'start';
       } else if (i === ticksCount - 1) {
         tickValue.style.textAnchor = 'end';
+      } else {
+        tickValue.style.textAnchor = 'middle';
       }
       tick.appendChild(tickValue);
     }
