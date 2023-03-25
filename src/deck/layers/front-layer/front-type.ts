9a7786bc5a134fe491d696a6b1895572479f6f78
Jan Zak <zj@zakjan.cz>
2023-03-25 09:29:42 +0100

    Fix front layer flickering

--- src/deck/layers/front-layer/front-type.ts
+++ src/deck/layers/front-layer/front-type.ts
@@ -8,4 +8,10 @@ export enum FrontType {
   STATIONARY = 'STATIONARY',
 }
 
+// icon anchor needs to be in the non-transparent part of the icon to workaround for CollisionFilterExtension flickering
+// see https://github.com/visgl/deck.gl/pull/7679
+// ->
+// icon height = 35
+// icon padding bottom = 3 (found experimentally, works best for rotated icons)
+// total icon height = 38
 export {iconAtlas, iconMapping};
