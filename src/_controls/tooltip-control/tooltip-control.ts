337fff8d5d4d7f545973db80db4f6e78324c8f80
Jan Zak <zj@zakjan.cz>
2023-10-14 21:40:34 +0200

    Add followCursor to TooltipControl

--- src/_controls/tooltip-control/tooltip-control.ts
+++ src/_controls/tooltip-control/tooltip-control.ts
@@ -1,3 +1,4 @@
+import type { PickingInfo } from '@deck.gl/core/typed';
 import { formatValueWithUnit, formatDirection } from '../../_utils/format.js';
 import type { UnitFormat } from '../../_utils/unit-format.js';
 import { RasterPointProperties } from '../../_utils/raster-data.js';
@@ -6,9 +7,11 @@ import './tooltip-control.css';
 
 export interface TooltipControlConfig {
   unitFormat: UnitFormat;
+  followCursor: boolean;
 }
 
 const CONTROL_CLASS = 'weatherlayers-tooltip-control';
+const FOLLOW_CURSOR_CLASS = 'follow-cursor';
 
 export class TooltipControl extends Control<TooltipControlConfig> {
   #config: TooltipControlConfig;
@@ -60,9 +63,10 @@ export class TooltipControl extends Control<TooltipControlConfig> {
     this.#config = config;
 
     this.#container.innerHTML = '';
+    this.#container.classList.toggle(FOLLOW_CURSOR_CLASS, this.#config.followCursor);
   }
 
-  update(rasterPointProperties?: RasterPointProperties): void {
+  update(rasterPointProperties: RasterPointProperties | undefined): void {
     if (!this.#container) {
       return;
     }
@@ -71,15 +75,61 @@ export class TooltipControl extends Control<TooltipControlConfig> {
       this.#container.innerHTML = '';
       return;
     }
-    
-    const unitFormat = this.#config.unitFormat;
 
     const { value, direction } = rasterPointProperties;
-    let tooltip = formatValueWithUnit(value, unitFormat);
+    if (typeof value === 'undefined') {
+      this.#container.innerHTML = '';
+      return;
+    }
+  
+    let tooltip = formatValueWithUnit(value, this.#config.unitFormat);
     if (typeof direction !== 'undefined') {
       tooltip += `, ${formatDirection(direction)}`
     }
 
     this.#container.innerHTML = `<div>${tooltip}</div>`;
+  }
+
+  updatePickingInfo(pickingInfo: PickingInfo & { raster?: RasterPointProperties }): void {
+    if (!this.#container) {
+      return;
+    }
+
+    if (!pickingInfo) {
+      this.#container.innerHTML = '';
+      return;
+    }
+
+    this.update(pickingInfo.raster);
+
+    if (this.#config.followCursor) {
+      // update position
+      const width = pickingInfo?.viewport?.width;
+      const height = pickingInfo?.viewport?.height;
+      if (width && height) {
+        if (pickingInfo.x < width / 2) {
+          this.#container.style.left = `${pickingInfo.x}px`;
+          this.#container.style.right = '';
+        } else {
+          this.#container.style.left = '';
+          this.#container.style.right = `${width - pickingInfo.x}px`;
+        }
+
+        if (pickingInfo.y < height / 2) {
+          this.#container.style.top = `${pickingInfo.y}px`;
+          this.#container.style.bottom = '';
+        } else {
+          this.#container.style.top = '';
+          this.#container.style.bottom = `${height - pickingInfo.y}px`;
+        }
+      }
+
+      // hide on panning
+      document.addEventListener('mousedown', () => {
+        if (this.#container) {
+          this.#container.innerHTML = '';
+        }
+      }, { once: true });
+    }
   }
 }
