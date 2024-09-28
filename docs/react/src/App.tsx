35c6b2915bdfa9398c4ad38ab4658a521538b859
Jan Zak <zj@zakjan.cz>
2024-09-28 12:55:57 +0200

    Add React Mapbox demo

--- docs/react/src/App.tsx
+++ docs/react/src/App.tsx
@@ -1,9 +1,18 @@
+import FPSStats from 'react-fps-stats';
+
 import Deck from './Deck.tsx';
 import Maplibre from './Maplibre.tsx';
+import Mapbox from './Mapbox.tsx';
 
 export default function App() {
   return (
-    // <Deck />
-    <Maplibre />
+    <div className="relative w-full h-full">
+      {/*
+      <Deck />
+      <Maplibre />
+      */}
+      <Mapbox />
+      <FPSStats top='auto' right={0} bottom={0} left='auto' />
+    </div>
   );
 }
---
+++ docs/react/src/Mapbox.tsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from 'react';
+import { Map, useControl } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { MapView } from 'deck.gl';
+import type { DeckProps } from 'deck.gl';
+import { MapboxOverlay } from '@deck.gl/mapbox';
+import { ClipExtension } from '@deck.gl/extensions';
+import { setLicense, offsetDatetimeRange, RasterLayer, ParticleLayer } from 'weatherlayers-gl';
+import { Client } from 'weatherlayers-gl/client';
+import type { Dataset, DatasetData } from 'weatherlayers-gl/client';
+
+import { WEATHER_LAYERS_ACCESS_TOKEN, MAPBOX_ACCESS_TOKEN } from '../../auth.js';
+import { BASEMAP_VECTOR_STYLE_URL } from '../../basemap.js';
+import LICENSE from '../../license.json';
+
+const DATASET = 'gfs/wind_10m_above_ground';
+const CURRENT_DATETIME = new Date().toISOString();
+
+setLicense(LICENSE);
+const client = new Client({
+  accessToken: WEATHER_LAYERS_ACCESS_TOKEN,
+});
+
+function DeckGLOverlay(props: DeckProps) {
+  const overlay = useControl<MapboxOverlay>(() => new MapboxOverlay(props));
+  overlay.setProps(props);
+  return null;
+}
+
+export default function Maplibre() {
+  const [dataset, setDataset] = useState<Dataset | null>(null);
+  const [datasetData, setDatasetData] = useState<DatasetData | null>(null);
+
+  useEffect(() => {
+    const load = async () => {
+      const dataset = await client.loadDataset(DATASET);
+      const {datetimes} = await client.loadDatasetSlice(DATASET, offsetDatetimeRange(CURRENT_DATETIME, 0, 24));
+      const datetime = datetimes[0];
+      const datasetData = await client.loadDatasetData(DATASET, datetime);
+
+      setDataset(dataset);
+      setDatasetData(datasetData);
+    };
+
+    load();
+  }, []);
+
+  const {palette} = dataset ?? {};
+  const {image, imageType, imageUnscale, bounds} = datasetData ?? {};
+
+  return (
+    <Map
+      style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}
+      mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+      mapStyle={BASEMAP_VECTOR_STYLE_URL}
+      initialViewState={{
+        longitude: 30,
+        latitude: 10,
+        zoom: 0,
+      }}
+    >
+      <DeckGLOverlay
+        interleaved
+        views={new MapView({ repeat: true })}
+        controller={{ normalize: false }}
+        layers={[
+          new RasterLayer({
+            id: 'raster',
+            // data properties
+            image,
+            imageType,
+            imageUnscale,
+            bounds,
+            // style properties
+            visible: true,
+            palette,
+            opacity: 0.2,
+            pickable: true,
+            extensions: [new ClipExtension()],
+            clipBounds: [-181, -85.051129, 181, 85.051129],
+          }),
+          new ParticleLayer({
+            id: 'particle',
+            // data properties
+            image,
+            imageType,
+            imageUnscale,
+            bounds,
+            // style properties
+            visible: true,
+            numParticles: 5000,
+            maxAge: 10,
+            speedFactor: 10,
+            width: 2,
+            opacity: 0.2,
+            animate: true,
+            extensions: [new ClipExtension()],
+            clipBounds: [-180, -85.051129, 180, 85.051129],
+            getPolygonOffset: () => [0, -1000],
+          }),
+        ]}
+      />
+    </Map>
+  );
+}
