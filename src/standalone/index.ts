be3ad97b491b045e038db8b5d7322bea4662b696
Jan Zak <zj@zakjan.cz>
2025-01-20 22:54:02 +0100

    Fix rotation in globe demos, fix standalone demos

--- src/standalone/index.ts
+++ src/standalone/index.ts
@@ -2,6 +2,7 @@ export * from '../deck/_utils/index.js';
 export * from '../deck/controls/index.js';
 
 export {GridStyle} from '../deck/layers/grid-layer/grid-style.js'; // required by demo only
+export {DirectionType} from '../deck/_utils/direction-type.js'; // required by demo only
 
 export * from './providers/raster-provider/raster-provider.js';
 export * from './providers/contour-provider/contour-provider.js';
