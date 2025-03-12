b52534f0e58dbd8dd805aee22d5837be988cd664
Jan Zak <zj@zakjan.cz>
2025-03-13 00:40:44 +0100

    Update ECMWF wind names

--- docs/config.js
+++ docs/config.js
@@ -45,7 +45,7 @@ const GRID_LAYER_DATASET_CONFIG = {
   'hrrr_alaska/temperature_2m_above_ground': { style: WeatherLayers.GridStyle.VALUE },
   'ecmwf_ifs/wind_10m_above_ground': { style: WeatherLayers.GridStyle.WIND_BARB, iconBounds: [0, 100 * 0.51444] }, // 100 kts to m/s
   'ecmwf_ifs/wind_100m_above_ground': { style: WeatherLayers.GridStyle.WIND_BARB, iconBounds: [0, 100 * 0.51444] }, // 100 kts to m/s
-  'ecmwf_ifs/wind_gust_10m_above_ground': { style: WeatherLayers.GridStyle.WIND_BARB, iconBounds: [0, 100 * 0.51444] }, // 100 kts to m/s
+  'ecmwf_ifs/wind_gust_surface': { style: WeatherLayers.GridStyle.WIND_BARB, iconBounds: [0, 100 * 0.51444] }, // 100 kts to m/s
   'ecmwf_ifs/temperature_2m_above_ground': { style: WeatherLayers.GridStyle.VALUE },
   'ecmwf_aifs/wind_10m_above_ground': { style: WeatherLayers.GridStyle.WIND_BARB, iconBounds: [0, 100 * 0.51444] }, // 100 kts to m/s
   'ecmwf_aifs/wind_100m_above_ground': { style: WeatherLayers.GridStyle.WIND_BARB, iconBounds: [0, 100 * 0.51444] }, // 100 kts to m/s
@@ -84,7 +84,7 @@ const PARTICLE_LAYER_DATASET_CONFIG = {
   'hrrr_alaska/wind_gust_surface': { speedFactor: 3, width: 2 },
   'ecmwf_ifs/wind_10m_above_ground': { speedFactor: 3, width: 2 },
   'ecmwf_ifs/wind_100m_above_ground': { speedFactor: 3, width: 2 },
-  'ecmwf_ifs/wind_gust_10m_above_ground': { speedFactor: 3, width: 2 },
+  'ecmwf_ifs/wind_gust_surface': { speedFactor: 3, width: 2 },
   'ecmwf_aifs/wind_10m_above_ground': { speedFactor: 3, width: 2 },
   'ecmwf_aifs/wind_100m_above_ground': { speedFactor: 3, width: 2 },
   'cmems_phy/currents': { speedFactor: 50, width: 2 },
@@ -120,7 +120,7 @@ const TOOLTIP_CONTROL_DATASET_CONFIG = {
   'hrrr_alaska/wind_gust_surface': { directionType: WeatherLayers.DirectionType.INWARD },
   'ecmwf_ifs/wind_10m_above_ground': { directionType: WeatherLayers.DirectionType.INWARD },
   'ecmwf_ifs/wind_100m_above_ground': { directionType: WeatherLayers.DirectionType.INWARD },
-  'ecmwf_ifs/wind_gust_10m_above_ground': { directionType: WeatherLayers.DirectionType.INWARD },
+  'ecmwf_ifs/wind_gust_surface': { directionType: WeatherLayers.DirectionType.INWARD },
   'ecmwf_aifs/wind_10m_above_ground': { directionType: WeatherLayers.DirectionType.INWARD },
   'ecmwf_aifs/wind_100m_above_ground': { directionType: WeatherLayers.DirectionType.INWARD },
   'cmems_phy/currents': { directionType: WeatherLayers.DirectionType.OUTWARD },
