21507d336b37bce8b10239c890da74ab01678ada
Jan Žák <zj@zakjan.cz>
2025-11-17 19:17:20 +0100

    Update access token

--- docs/auth.js
+++ docs/auth.js
@@ -1,5 +1,5 @@
 // WeatherLayers access token
-export const WEATHER_LAYERS_ACCESS_TOKEN = '9djqrhlmAjv2Mv2z2Vwz';
+export const WEATHER_LAYERS_ACCESS_TOKEN = 'C0F3AFB7-47B3-4FED-A16F-CF4148F65E4A';
 
 // WeatherLayers mapbox-gl-js access token
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiemFramFuIiwiYSI6ImNrcG4xZjc0ZjRhNmMybm54NnRjcW1pejAifQ.wLXIYDVrD_6yQPszlIB8Dg';
