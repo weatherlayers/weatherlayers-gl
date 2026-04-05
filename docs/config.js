2d710d1122d89ada2b8b98a0235828f1e82b2d5e
Jan Žák <zj@zakjan.cz>
2026-04-05 21:14:01 +0200

    Use layer and control config from backend

--- docs/config.js
+++ docs/config.js
@@ -4,160 +4,12 @@ export const NO_DATA = 'no data';
 
 const DEFAULT_DATASET = 'gfs/wind_10m_above_ground';
 
-const DATASETS_WEATHER_WIND = [
-  'gfs/wind_10m_above_ground',
-  'gfs/wind_100m_above_ground',
-  ...[1000, 975, 950, 925, 900, 850, 800, 750, 700, 650, 600, 550, 500, 450, 400, 350, 300, 250, 200, 150, 100, 50].map(level => `gfs/wind_${level}hpa`),
-  'gfs/wind_tropopause',
-  'gfs/wind_gust_surface',
-  'hrrr_conus/wind_10m_above_ground',
-  'hrrr_conus/wind_80m_above_ground',
-  ...[1000, 975, 950, 925, 900, 850, 800, 750, 700, 650, 600, 550, 500, 450, 400, 350, 300, 250, 200, 150, 100, 50].map(level => `hrrr_conus/wind_${level}hpa`),
-  'hrrr_conus/wind_gust_surface',
-  'hrrr_alaska/wind_10m_above_ground',
-  'hrrr_alaska/wind_80m_above_ground',
-  ...[1000, 975, 950, 925, 900, 850, 800, 750, 700, 650, 600, 550, 500, 450, 400, 350, 300, 250, 200, 150, 100, 50].map(level => `hrrr_alaska/wind_${level}hpa`),
-  'hrrr_alaska/wind_gust_surface',
-  'ecmwf_ifs/wind_10m_above_ground',
-  'ecmwf_ifs/wind_100m_above_ground',
-  ...[1000, 925, 850, 700, 600, 500, 400, 300, 250, 200, 150, 100, 50].map(level => `ecmwf_ifs/wind_${level}hpa`),
-  'ecmwf_ifs/wind_gust_surface',
-  'ecmwf_aifs/wind_10m_above_ground',
-  'ecmwf_aifs/wind_100m_above_ground',
-  ...[1000, 925, 850, 700, 600, 500, 400, 300, 250, 200, 150, 100, 50].map(level => `ecmwf_aifs/wind_${level}hpa`),
-  'meteofrance_arpege/wind_10m_above_ground',
-  'meteofrance_arpege/wind_100m_above_ground',
-  'meteofrance_arpege/wind_gust_10m_above_ground',
-  'meteofrance_arpege_eu/wind_10m_above_ground',
-  'meteofrance_arpege_eu/wind_100m_above_ground',
-  'meteofrance_arpege_eu/wind_gust_10m_above_ground',
-  'meteofrance_arome/wind_10m_above_ground',
-  'meteofrance_arome/wind_100m_above_ground',
-  'meteofrance_arome/wind_gust_10m_above_ground',
-  'meteofrance_arome_hd/wind_10m_above_ground',
-  'meteofrance_arome_hd/wind_100m_above_ground',
-  'meteofrance_arome_hd/wind_gust_10m_above_ground',
-  'metoffice_global/wind_10m_above_ground',
-  'metoffice_global/wind_gust_10m_above_ground',
-  'metoffice_uk/wind_10m_above_ground',
-  'metoffice_uk/wind_gust_10m_above_ground',
-];
-const DATASETS_WEATHER_TEMPERATURE = [
-  'gfs/temperature_2m_above_ground',
-  'gfs/temperature_500mb',
-  'gfs/apparent_temperature_2m_above_ground',
-  'hrrr_conus/temperature_2m_above_ground',
-  'hrrr_conus/temperature_500mb',
-  'hrrr_alaska/temperature_2m_above_ground',
-  'hrrr_alaska/temperature_500mb',
-  'ecmwf_ifs/temperature_2m_above_ground',
-  'ecmwf_aifs/temperature_2m_above_ground',
-  'meteofrance_arpege/temperature_2m_above_ground',
-  'meteofrance_arpege_eu/temperature_2m_above_ground',
-  'meteofrance_arome/temperature_2m_above_ground',
-  'meteofrance_arome_hd/temperature_2m_above_ground',
-  'metoffice_global/temperature_surface',
-  'metoffice_uk/temperature_surface',
-];
-const DATASETS_WEATHER_PRESSURE = [
-  'gfs/pressure_mean_sea_level',
-  'hrrr_conus/pressure_mean_sea_level',
-  'hrrr_alaska/pressure_mean_sea_level',
-  'ecmwf_ifs/pressure_mean_sea_level',
-  'ecmwf_aifs/pressure_mean_sea_level',
-  'meteofrance_arpege/pressure_mean_sea_level',
-  'meteofrance_arpege_eu/pressure_mean_sea_level',
-  'meteofrance_arome/pressure_mean_sea_level',
-  'meteofrance_arome_hd/pressure_mean_sea_level',
-  'metoffice_global/pressure_mean_sea_level',
-  'metoffice_uk/pressure_mean_sea_level',
-];
-const DATASETS_OCEAN_SEA_SURFACE_TEMPERATURE = [
-  'cmems_sst/sea_surface_temperature',
-];
-const DATASETS_OCEAN_WAVES = [
-  'gfswave/waves',
-  'gfswave/swell',
-  'gfswave/swell2',
-  'gfswave/swell3',
-  'meteofrance_mfwam/waves',
-  'meteofrance_mfwam/swell',
-  'meteofrance_mfwam/swell2',
-  'meteofrance_mfwam_fr/waves',
-  'meteofrance_mfwam_fr/swell',
-  'meteofrance_mfwam_fr/swell2',
-  'cmems_wav/waves',
-  'cmems_wav/swell',
-  'cmems_wav/swell2',
-  'cmems_wav/swell3',
-  'cmems_wav_med/waves',
-  'cmems_wav_med/swell',
-  'cmems_wav_med/swell2',
-  'cmems_wav_med/swell3',
-  'cmems_wav_nws/waves',
-  'cmems_wav_nws/swell',
-  'cmems_wav_nws/swell2',
-  'cmems_wav_nws/swell3',
-];
-const DATASETS_OCEAN_CURRENTS = [
-  'cmems_phy/currents',
-  'cmems_phy_merged/currents',
-  'cmems_phy_merged/tidal_currents',
-  'cmems_phy_merged/total_currents',
-  'cmems_phy_med/currents',
-  'cmems_phy_nws/currents',
-];
-
-const CONTOUR_LAYER_DATASET_WEATHER_TEMPERATURE = { interval: 2, majorInterval: 10 };
-const CONTOUR_LAYER_DATASET_WEATHER_PRESSURE = { interval: 2, majorInterval: 10 };
-const CONTOUR_LAYER_DATASET_OCEAN_SEA_SURFACE_TEMPERATURE = { interval: 2, majorInterval: 10 };
-const HIGH_LOW_LAYER_DATASET_WEATHER_PRESSURE = { radius: 2000 };
-const GRID_LAYER_DATASET_WEATHER_WIND = { style: WeatherLayers.GridStyle.WIND_BARB, iconBounds: [0, 100 * 0.51444] }; // 100 kts to m/s
-const GRID_LAYER_DATASET_WEATHER_TEMPERATURE = { style: WeatherLayers.GridStyle.VALUE };
-const GRID_LAYER_DATASET_OCEAN_SEA_SURFACE_TEMPERATURE = { style: WeatherLayers.GridStyle.VALUE };
-const GRID_LAYER_DATASET_OCEAN_WAVES = { style: WeatherLayers.GridStyle.ARROW, iconBounds: [0, 35] };
-const GRID_LAYER_DATASET_OCEAN_CURRENTS = { style: WeatherLayers.GridStyle.ARROW, iconBounds: [0, 3] };
-const PARTICLE_LAYER_DATASET_WEATHER_WIND = { speedFactor: 3, width: 2 };
-const PARTICLE_LAYER_DATASET_OCEAN_WAVES = { speedFactor: 2, width: 5 };
-const PARTICLE_LAYER_DATASET_OCEAN_CURRENTS = { speedFactor: 50, width: 2 };
-const TOOLTIP_CONTROL_DATASET_WEATHER_WIND = { directionType: WeatherLayers.DirectionType.INWARD };
-const TOOLTIP_CONTROL_DATASET_OCEAN_WAVES = { directionType: WeatherLayers.DirectionType.INWARD };
-const TOOLTIP_CONTROL_DATASET_OCEAN_CURRENTS = { directionType: WeatherLayers.DirectionType.OUTWARD };
-
-const CONTOUR_LAYER_DATASET_CONFIG = {
-  ...(Object.fromEntries(DATASETS_WEATHER_TEMPERATURE.map(dataset => [dataset, CONTOUR_LAYER_DATASET_WEATHER_TEMPERATURE]))),
-  ...(Object.fromEntries(DATASETS_WEATHER_PRESSURE.map(dataset => [dataset, CONTOUR_LAYER_DATASET_WEATHER_PRESSURE]))),
-  ...(Object.fromEntries(DATASETS_OCEAN_SEA_SURFACE_TEMPERATURE.map(dataset => [dataset, CONTOUR_LAYER_DATASET_OCEAN_SEA_SURFACE_TEMPERATURE]))),
-};
-const HIGH_LOW_LAYER_DATASET_CONFIG = {
-  ...(Object.fromEntries(DATASETS_WEATHER_PRESSURE.map(dataset => [dataset, HIGH_LOW_LAYER_DATASET_WEATHER_PRESSURE]))),
-};
-const GRID_LAYER_DATASET_CONFIG = {
-  ...(Object.fromEntries(DATASETS_WEATHER_WIND.map(dataset => [dataset, GRID_LAYER_DATASET_WEATHER_WIND]))),
-  ...(Object.fromEntries(DATASETS_WEATHER_TEMPERATURE.map(dataset => [dataset, GRID_LAYER_DATASET_WEATHER_TEMPERATURE]))),
-  ...(Object.fromEntries(DATASETS_OCEAN_SEA_SURFACE_TEMPERATURE.map(dataset => [dataset, GRID_LAYER_DATASET_OCEAN_SEA_SURFACE_TEMPERATURE]))),
-  ...(Object.fromEntries(DATASETS_OCEAN_WAVES.map(dataset => [dataset, GRID_LAYER_DATASET_OCEAN_WAVES]))),
-  ...(Object.fromEntries(DATASETS_OCEAN_CURRENTS.map(dataset => [dataset, GRID_LAYER_DATASET_OCEAN_CURRENTS]))),
-};
-const PARTICLE_LAYER_DATASET_CONFIG = {
-  ...(Object.fromEntries(DATASETS_WEATHER_WIND.map(dataset => [dataset, PARTICLE_LAYER_DATASET_WEATHER_WIND]))),
-  ...(Object.fromEntries(DATASETS_OCEAN_WAVES.map(dataset => [dataset, PARTICLE_LAYER_DATASET_OCEAN_WAVES]))),
-  ...(Object.fromEntries(DATASETS_OCEAN_CURRENTS.map(dataset => [dataset, PARTICLE_LAYER_DATASET_OCEAN_CURRENTS]))),
-};
-const TOOLTIP_CONTROL_DATASET_CONFIG = {
-  ...(Object.fromEntries(DATASETS_WEATHER_WIND.map(dataset => [dataset, TOOLTIP_CONTROL_DATASET_WEATHER_WIND]))),
-  ...(Object.fromEntries(DATASETS_OCEAN_WAVES.map(dataset => [dataset, TOOLTIP_CONTROL_DATASET_OCEAN_WAVES]))),
-  ...(Object.fromEntries(DATASETS_OCEAN_CURRENTS.map(dataset => [dataset, TOOLTIP_CONTROL_DATASET_OCEAN_CURRENTS]))),
-};
-
 const CURRENT_DATETIME = new Date().toISOString();
 
 export async function initConfig({ datasets, deckgl, webgl2, globe } = {}) {
-  const urlConfig = new URLSearchParams(location.hash.substring(1));
-
   const config = {
     datasets: datasets ?? [],
-    dataset: urlConfig.get('dataset') ?? DEFAULT_DATASET,
+    dataset: DEFAULT_DATASET,
     datetimeRange: WeatherLayers.offsetDatetimeRange(CURRENT_DATETIME, 0, 24).join('/'),
     datetimeStep: 3,
     datetimes: [],
@@ -234,8 +86,8 @@ export async function initConfig({ datasets, deckgl, webgl2, globe } = {}) {
         enabled: false,
         numParticles: 5000,
         maxAge: 10,
-        speedFactor: 0, // dataset-specific
-        width: 0, // dataset-specific
+        speedFactor: 3, // dataset-specific
+        width: 2, // dataset-specific
         color: colorToCss(WeatherLayers.DEFAULT_LINE_COLOR),
         palette: false,
         opacity: 0.2,
@@ -250,7 +102,7 @@ export async function initConfig({ datasets, deckgl, webgl2, globe } = {}) {
     },
   };
 
-  loadUrlConfig(config, { deckgl, webgl2 });
+  loadUrlConfig(config);
 
   return config;
 }
@@ -271,57 +123,14 @@ function getDatetimeOptions(datetimes) {
   return datetimes.map(x => ({ value: x, text: WeatherLayers.formatDatetime(x) }));
 }
 
-function loadUrlConfig(config, { deckgl, webgl2 } = {}) {
+function loadUrlConfig(config) {
   const urlConfig = new URLSearchParams(location.hash.substring(1));
-
-  config.raster.enabled = urlConfig.has('raster') ? urlConfig.get('raster') === 'true' : true;
-
-  config.contour.enabled = urlConfig.has('contour') ? urlConfig.get('contour') === 'true' : !!CONTOUR_LAYER_DATASET_CONFIG[config.dataset];
-  config.contour.interval = CONTOUR_LAYER_DATASET_CONFIG[config.dataset]?.interval || 2;
-  config.contour.majorInterval = CONTOUR_LAYER_DATASET_CONFIG[config.dataset]?.majorInterval || 10;
-
-  config.highLow.enabled = urlConfig.has('highLow') ? urlConfig.get('highLow') === 'true' : !!HIGH_LOW_LAYER_DATASET_CONFIG[config.dataset];
-  config.highLow.radius = HIGH_LOW_LAYER_DATASET_CONFIG[config.dataset]?.radius || 2000;
-
-  if (deckgl) {
-    config.grid.enabled = urlConfig.has('grid') ? urlConfig.get('grid') === 'true' : !!GRID_LAYER_DATASET_CONFIG[config.dataset];
-    config.grid.style = GRID_LAYER_DATASET_CONFIG[config.dataset]?.style || WeatherLayers.GridStyle.VALUE;
-    config.grid.iconBounds = GRID_LAYER_DATASET_CONFIG[config.dataset]?.iconBounds || null;
-  }
-
-  if (webgl2) {
-    config.particle.enabled = urlConfig.has('particle') ? urlConfig.get('particle') === 'true' : !!PARTICLE_LAYER_DATASET_CONFIG[config.dataset];
-    config.particle.speedFactor = PARTICLE_LAYER_DATASET_CONFIG[config.dataset]?.speedFactor || 0;
-    config.particle.width = PARTICLE_LAYER_DATASET_CONFIG[config.dataset]?.width || 0;
-  }
-
-  config.tooltip.directionType = TOOLTIP_CONTROL_DATASET_CONFIG[config.dataset]?.directionType || WeatherLayers.DirectionType.INWARD;
+  config.dataset = urlConfig.get('dataset') ?? DEFAULT_DATASET;
 }
 
-function updateUrlConfig(config, { deckgl, webgl2 } = {}) {
+function updateUrlConfig(config) {
   const urlConfig = new URLSearchParams();
-  if (config.dataset !== DEFAULT_DATASET) {
-    urlConfig.set('dataset', config.dataset);
-  }
-  if (config.raster.enabled !== true) {
-    urlConfig.set('raster', config.raster.enabled);
-  }
-  if (config.contour.enabled !== !!CONTOUR_LAYER_DATASET_CONFIG[config.dataset]) {
-    urlConfig.set('contour', config.contour.enabled);
-  }
-  if (config.highLow.enabled !== !!HIGH_LOW_LAYER_DATASET_CONFIG[config.dataset]) {
-    urlConfig.set('highLow', config.highLow.enabled);
-  }
-  if (deckgl) {
-    if (config.grid.enabled !== !!GRID_LAYER_DATASET_CONFIG[config.dataset]) {
-      urlConfig.set('grid', config.grid.enabled);
-    }
-  }
-  if (webgl2) {
-    if (config.particle.enabled !== !!PARTICLE_LAYER_DATASET_CONFIG[config.dataset]) {
-      urlConfig.set('particle', config.particle.enabled);
-    }
-  }
+  urlConfig.set('dataset', config.dataset);
   window.history.replaceState(null, null, '#' + urlConfig.toString());
 }
 
@@ -336,18 +145,16 @@ function debounce(callback, wait) {
 }
 
 export function initGui(config, update, { deckgl, webgl2, globe } = {}) {
-  const originalUpdate = update;
-  update = debounce(() => { updateUrlConfig(config, { deckgl, webgl2 }); originalUpdate() }, 100);
-  const updateLast = event => event.last && update();
-
   const gui = new Pane();
+
+  const originalUpdate = update;
+  update = debounce(() => { originalUpdate(gui) }, 100);
+  const updateLast = event => event.last && update();
 
   let datetime;
   const updateDatetimes = async () => {
-    // force update dataset
-    await originalUpdate(true);
-    loadUrlConfig(config, { deckgl, webgl2 });
-    updateUrlConfig(config, { deckgl, webgl2 });
+    await originalUpdate();
+    updateUrlConfig(config);
 
     // update datetimes
     datetime.dispose();
@@ -355,7 +162,7 @@ export function initGui(config, update, { deckgl, webgl2, globe } = {}) {
     gui.refresh();
 
     // force update datetime
-    originalUpdate(true);
+    originalUpdate(gui, true);
   };
 
   gui.addBinding(config, 'dataset', { options: getOptions([NO_DATA, ...config.datasets]) }).on('change', updateDatetimes);
